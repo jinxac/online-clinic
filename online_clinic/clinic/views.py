58606a860b317ad7c4e21ce4e4e8aa4f0d3e7645
salujaharkirat <saluja.harkirat@gmail.com>
2020-11-18 19:48:11 +0530

    adding register user api

--- online_clinic/clinic/views.py
+++ online_clinic/clinic/views.py
@@ -1,13 +1,43 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.status import HTTP_201_CREATED
+from django.contrib.auth.models import User
 
 
-from .models import Clinic
+
+from .models import Clinic,Role, Employee, EmployeeRole
 from .serializer import ClinicSerializer
 
 from rest_framework.permissions import IsAdminUser
 
 # Create your views here.
+@api_view(['POST'])
+def register_user(request):
+  user = User.objects.create(
+    email=request.data['email'],
+    username=request.data['username']
+  )
+  user.set_password(request.data['password'])
+
+  role = Role.objects.get(role_name=request.data['role'])
+
+  employee = Employee.objects.create(
+    user=user,
+    first_name=request.data['first_name'],
+    last_name=request.data['last_name'],
+    gender=request.data['gender'],
+    phone_number=request.data['phone_number'],
+    permanent_address=request.data['permanent_address']
+  )
+
+  EmployeeRole.objects.create(
+    employee=employee,
+    role=role
+  )
+
+  return Response({"message": "Hello, world!", "status": HTTP_201_CREATED})
 
 class ClinicList(generics.ListCreateAPIView):
   serializer_class = ClinicSerializer
--- online_clinic/urls.py
+++ online_clinic/urls.py
@@ -21,7 +21,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from online_clinic.clinic.views import ClinicList
+from online_clinic.clinic.views import ClinicList, register_user
 
 
 urlpatterns = [
@@ -29,4 +29,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v0/clinics/', ClinicList.as_view(), name='offices'),
+    path('api/v0/register/', register_user, name='register'),
 ]
